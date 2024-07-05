15f870ddc5def053b0464d73b4f04c5e959b73cb
dev <dev@A32PC10>
2024-07-05 17:47:11 +0300

    Add project files.

---
+++ Data/HotelDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hotel.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("conection...str");
+
+        }
+    }
+}
