7dc767d5ffc7a9dcaeb661139ad2503530941aa6
dev <dev@A32PC8>
2024-07-12 17:45:41 +0300

    add view create

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -1,4 +1,5 @@
 ﻿using hotel.Data;
+using hotel.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hotel.Controllers
@@ -20,5 +21,34 @@ namespace hotel.Controllers
             return View(rooms);
         }
 
+        public IActionResult Delete(int id)
+        {
+            var product = context.HotelRooms.Find(id);
+
+            if (product == null) return NotFound(); // 404
+
+            context.HotelRooms.Remove(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Catalog");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - create object in db
+        [HttpPost]
+
+        public IActionResult Create(HotelRoom model)
+        {   
+            if (!ModelState.IsValid) return View(model);
+
+            context.HotelRooms.Add(model);
+            context.SaveChanges();
+
+            return RedirectToAction("Catalog");
+        }
     }
 }
